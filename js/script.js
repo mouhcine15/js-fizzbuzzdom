fb3a963051ff8229e3f0c5b0484798ce7ee94f86
mouhcine <mouhcine15.00@gmail.com>
2022-05-05 16:06:22 +0200

    console log iniziali

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// stampare tutti i numri da 1 a 1
+for (let i = 1 ; i <= 100; i++) {
+
+    console.log(i);
+
+    // se multipli sia di 3 sia 5 stampare FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+
+    // se multipli di 5 stampare Buzz
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    
+    
+    // se multipli di 3 stampare fizz
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+
+
+
+}

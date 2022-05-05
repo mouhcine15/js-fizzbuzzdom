ffdbbc1ed66f3a93726f8d52c55a15dcf4b7ba63
mouhcine <mouhcine15.00@gmail.com>
2022-05-05 20:11:56 +0200

    stampa colori

--- js/script.js
+++ js/script.js
@@ -8,23 +8,23 @@ for (let i = 1 ; i <= 100; i++) {
     // se multipli sia di 3 sia 5 stampare FizzBuzz
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
-        element = `<div class="quadrato ${i}"> ${"BuzzFizz"}</div>`;
+        element = `<div class="quadrato red ${i}"> ${"BuzzFizz"}</div>`;
 
 
     // se multipli di 5 stampare Buzz
     } else if (i % 5 === 0) {
         console.log("Buzz");
-        element = `<div class="quadrato ${i}"> ${"Buzz"}</div>`;
+        element = `<div class="quadrato yellow ${i}"> ${"Buzz"}</div>`;
     
     
     // se multipli di 3 stampare fizz
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        element = `<div class="quadrato ${i}"> ${"Fizz"}</div>`;
+        element = `<div class="quadrato green ${i}"> ${"Fizz"}</div>`;
 
     } else {
         console.log(i);
-        element = `<div class="quadrato ${i}"> ${i}</div>`;
+        element = `<div class="quadrato blue ${i}"> ${i}</div>`;
     }
     squares.innerHTML += element;
 }
